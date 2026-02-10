1fee57f8858a4fcd10521462478252c23754e698
kamenov69 <kamen.p.kamenov@gmail.com>
2026-02-10 10:31:35 +0200

    renamed dirs + python sources

---
+++ pioArduinoProject/src/main.cpp
@@ -0,0 +1,33 @@
+
+
+/* v0.2
+
+   main.cpp - Main loop for Arduino
+   Part of SMILE project -
+
+*/
+
+
+#include <Arduino.h>
+#include "board.h"
+#include "globalVars.h"
+
+#include "ps_data.h"
+
+
+void setup() {
+  setup_board();
+  setup_ps_data();
+
+  //add_new_global_var("glo", 1, 0, 10);
+  //add_new_global_var("glo1", 7, 10, 10);
+  setup_var_list_cmd();
+}
+
+void loop() {
+  
+  loop_board();
+  loop_ps_data();
+
+}
+
